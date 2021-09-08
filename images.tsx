e2721deedf88b20343b777fdaa37463315f3a6ae
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2021-09-08 16:10:33 -0300

    Delete images.tsx

--- images.tsx
+++
@@ -1,6 +0,0 @@
-export const images = [
-    "http://http2.mlstatic.com/D_978173-MLA32582347639_102019-O.jpg",
-    "http://http2.mlstatic.com/D_754004-MLA32299823528_092019-O.jpg",
-    "http://http2.mlstatic.com/D_616691-MLA27900381255_082018-O.jpg",
-    "http://http2.mlstatic.com/D_918230-MLA31424182281_072019-O.jpg"
-];
